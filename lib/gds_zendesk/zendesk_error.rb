c71b8a1ef9a49ee69be21a0bacbe4997de7eeed2
Jake Benilov <benilov@gmail.com>
2013-01-11 17:56:41 +0000

    extending the Zendesk exception
    ZendeskError is now able to store and pass through the underlying
    error from Zendesk

--- lib/gds_zendesk/zendesk_error.rb
+++ lib/gds_zendesk/zendesk_error.rb
@@ -1,3 +1,11 @@
 module GDSZendesk
-  class ZendeskError < StandardError; end
+  class ZendeskError < StandardError
+    attr_reader :underlying_error_details_from_zendesk
+
+    def initialize(message, underlying_error_details_from_zendesk = nil)
+      super(message)
+
+      @underlying_error_details_from_zendesk = underlying_error_details_from_zendesk
+    end
+  end
 end
