a114506b3a3ae77bc248575322b54c0ce87ea2dd
Jake Benilov <benilov@gmail.com>
2013-01-16 17:53:52 +0000

    merging ClientBuilder => Client

--- spec/gds_zendesk/client_spec.rb
+++ spec/gds_zendesk/client_spec.rb
@@ -1,17 +1,72 @@
 require 'gds_zendesk/client'
+require 'gds_zendesk/zendesk_error'
+require 'null_logger'
 
 module GDSZendesk
   describe Client do
-    let(:builder) { stub("builder") }
-    let(:client) { stub("client") }
+    let(:valid_credentials) { { username: "user", password: "pass" } }
 
-    it "should pass options to ClientBuilder and build a client" do
-      options = { something: true }
+    it "should raise an error if no username is provided" do
+      options = { password: "abc" }
 
-      ClientBuilder.should_receive(:new).with(something: true).and_return(builder)
-      builder.should_receive(:build).and_return(client)
+      lambda { Client.new(options) }.should raise_error(ArgumentError,
+                                                        /username not provided/)
+    end
 
-      Client.new(options)
+    it "should raise an error if no password is provided" do
+      options = { username: "abc" }
+
+      lambda { Client.new(options) }.should raise_error(ArgumentError,
+                                                        /password not provided/)
+    end
+
+    it "should use a null logger if no logger has been provided" do
+      Client.new(valid_credentials).config_options[:logger].should be_an_instance_of(NullLogger::Logger)
+    end
+
+    it "should use the passed logger if one has been provided" do
+      custom_logger = stub("logger")
+
+      Client.new(valid_credentials.merge(logger: custom_logger)).config_options[:logger].should eq(custom_logger)
+    end
+
+    context "upon a response from the Zendesk API" do
+      let(:custom_logger) { stub("logger").as_null_object }
+      let(:valid_options) { {logger: custom_logger, username: "user", password: "pass"} }
+      let(:builder) { Client.new(valid_options) }
+
+      context "a successful response from zendesk" do
+        it "should log the respose" do
+          successful_response = { status: 200, body: "OK" }
+          custom_logger.should_receive(:info).with(successful_response)
+
+          builder.callback(successful_response)
+        end
+      end
+
+      context "a response that the request isn't authorised" do
+        it "should raise an error" do
+          lambda {
+            builder.callback(status: 401, body: {"error" => "Unauth" })
+          }.should raise_error(ZendeskError, /Authentication Error/)
+        end
+      end
+
+      context "a response that the account has been locked out" do
+        it "should raise an error" do
+          response = { status: 403, body: "Too many failed login attempts for user ..." }
+          lambda { builder.callback(response) }.should raise_error(ZendeskError, 
+                                                                   /Authentication Error/)
+        end
+      end
+
+      context "a response that some zendesk validation failed" do
+        it "should raise an error" do
+          response = { status: 422, body: {"error" => "Some validation failure" } }
+          lambda { builder.callback(response) }.should raise_error(ZendeskError, 
+                                                                   /Error creating ticket/)
+        end
+      end
     end
   end
 end
