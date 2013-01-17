211d6ebc3c598baf7fe8afc1d9c8b5285a510029
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-01-17 12:21:51 +0000

    Invoke gem publisher on successful build

--- jenkins.sh
+++ jenkins.sh
@@ -3,5 +3,5 @@ set -e
 rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake
-# publishing of the gem disabled for the moment, gem is taken from github directly by bundler
-# bundle exec rake publish_gem
+publishing of the gem disabled for the moment, gem is taken from github directly by bundler
+bundle exec rake publish_gem
