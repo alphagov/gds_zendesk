67a63767396d87150ea6ba9c55128b1858ad9740
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-01-17 12:22:16 +0000

    Doh!

--- jenkins.sh
+++ jenkins.sh
@@ -3,5 +3,4 @@ set -e
 rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake
-publishing of the gem disabled for the moment, gem is taken from github directly by bundler
 bundle exec rake publish_gem
