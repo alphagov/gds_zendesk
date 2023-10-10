115f62f6c843352030a76722cdd52f0ba0f0ff45
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2023-10-10 17:19:29 +0100

    Fix broken WebMock stubs
    A prior release of the wrapped zendesk_api appears to have changed how pagination works(?), which broke our WebMock stubs

--- lib/gds_zendesk/test_helpers.rb
+++ lib/gds_zendesk/test_helpers.rb
@@ -3,14 +3,14 @@ require "json"
 module GDSZendesk
   module TestHelpers
     def zendesk_has_no_user_with_email(email)
-      stub_request(:get, "#{zendesk_endpoint}/users/search?page%5Bsize%5D=100&query=#{email}")
+      stub_request(:get, "#{zendesk_endpoint}/users/search?query=#{email}")
         .with(basic_auth: basic_auth_credentials)
         .to_return(body: { users: [], previous_page: nil, next_page: nil, count: 0 }.to_json,
                    headers: { "Content-Type" => "application/json" })
     end
 
     def zendesk_has_user(user_details)
-      stub_request(:get, "#{zendesk_endpoint}/users/search?page%5Bsize%5D=100&query=#{user_details[:email]}")
+      stub_request(:get, "#{zendesk_endpoint}/users/search?query=#{user_details[:email]}")
         .with(basic_auth: basic_auth_credentials)
         .to_return(body: { users: [user_details], previous_page: nil, next_page: nil, count: 1 }.to_json,
                    headers: { "Content-Type" => "application/json" })
